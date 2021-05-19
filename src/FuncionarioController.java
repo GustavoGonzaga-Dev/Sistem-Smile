c7574ef85c36af694232f9afe91f9d71247ff6aa
gustavo gonzaga <gustavogonzagafarias@gmail.com>
2021-05-19 17:44:46 -0300

    Adicionando validações

--- src/FuncionarioController.java
+++ src/FuncionarioController.java
@@ -1,7 +1,8 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FuncionarioController {
+public class FuncionarioController extends Component {
 
     private List<Funcionario> funcionarios = new ArrayList<>();
 
@@ -18,10 +19,40 @@ public class FuncionarioController {
         return  null;
     }
 
-    public boolean validarSenha(String senha, String confSenha){
-        if (senha.equals(confSenha)){
+    public void excluirPorCodigo(long codigo){
+        for (Funcionario Fn : funcionarios){
+            if(Fn.getCodigo() == codigo){
+                funcionarios.remove(Fn);
+            }
+        }
+    }
+
+    public boolean validarCampos(String txtNome, String txtEmail, String txtConfEmail, String txtCodigo, String cbPermissao, String txtSenha, String txtconfSenha){
+        List<String> lista = new ArrayList<>();
+        lista.add(0, txtNome);
+        lista.add(1, txtEmail);
+        lista.add(2, txtConfEmail);
+        lista.add(3, txtCodigo);
+        lista.add(4, cbPermissao);
+        lista.add(5, txtSenha);
+        lista.add(6, txtconfSenha);
+
+        for (String l : lista) {
+            if (l.equals(null) || l.equals("")){
+                lista.clear();
+                return false;
+            }
+        }
+        lista.clear();
+        return validarEmailSenha(txtEmail, txtConfEmail, txtSenha, txtconfSenha);
+    }
+
+    public boolean validarEmailSenha(String txtEmail, String txtConfEmail, String txtsenha, String txtconfSenha){
+        if (txtsenha.equals(txtconfSenha) && txtEmail.equals(txtConfEmail)){
             return true;
         }
         return false;
     }
+
+
 }
