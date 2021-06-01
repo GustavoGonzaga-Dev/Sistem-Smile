5939a11474923cfd8df8094bf9bb62c3c24b491f
Diego <Diegokoga1@gmail.com>
2021-05-31 23:31:40 -0300

    Tela menu sem funcionalidade

--- src/menu/MenuBoundary.java
+++ src/menu/MenuBoundary.java
@@ -1,4 +1,52 @@
 package menu;
 
-public class MenuBoundary {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class MenuBoundary extends Application {
+
+    private TextField txtBuscar = new TextField();
+
+    private Button btnEstoque = new Button("Estoque");
+    private Button btnFuncionario = new Button("Gerenciar \n" + "Funcionario");
+    private Button btnCaixa = new Button("Caixa");
+    private Button btnVenda = new Button("Venda");
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane borderPaine = new BorderPane();
+        GridPane painel = new GridPane();
+        Scene scene = new Scene(borderPaine, 500,330);
+
+        painel.getStylesheets().add(MenuBoundary.class.getResource("StylesMenu.css").toExternalForm());
+
+        borderPaine.getChildren().add(painel);
+        borderPaine.setTop(txtBuscar);
+        BorderPane.setAlignment(txtBuscar, Pos.CENTER);
+        painel.add(btnEstoque, 0,1);
+        painel.add(btnFuncionario,1,1);
+        painel.add(btnVenda,0,2);
+        painel.add(btnCaixa,1,2);
+        painel.setVgap(30);
+        painel.setHgap(50);
+        painel.setTranslateX(125);
+        painel.setTranslateY(30);
+
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.setTitle("Menu S2");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(MenuBoundary.class, args);
+    }
+
 }
---
+++ src/menu/MenuController.java
@@ -0,0 +1,18 @@
+package menu;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MenuController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
+
+    public void mudaTela(ActionEvent event){
+
+    }
+
+}
