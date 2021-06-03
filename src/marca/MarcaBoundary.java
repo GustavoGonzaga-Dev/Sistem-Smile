4927475da2ceb8c8f6afb1aa7be2e5fd5a218977
gustavo gonzaga <gustavogonzagafarias@gmail.com>
2021-06-02 23:31:36 -0300

    marca funcionando no banco

--- src/marca/MarcaBoundary.java
+++ src/marca/MarcaBoundary.java
@@ -1,13 +1,14 @@
 package marca;
 
+import funcionario.FuncionarioBoundary;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import login.Login;
 
 public class MarcaBoundary extends Application {
 
@@ -16,11 +17,17 @@ public class MarcaBoundary extends Application {
     private TextField txtPesquisar = new TextField();
 
     private Button btnPesquisar = new Button("Pesquisar");
-    private Button btnAdd = new Button("Add");
-    private Button btnRmv = new Button("Rmv");
-    private Button btnAlt = new Button("Alt");
+    private Button btnAdd = new Button("Adicionar");
+    private Button btnRmv = new Button("Remover");
+    private Button btnAlt = new Button("Alterar");
+    private Button btnCon = new Button("Concluir");
+    private Button btnCan = new Button("Cancelar");
 
     private MarcaController marcaControl = new MarcaController();
+    private boolean valido = false;
+
+    private Alert alertWarn = new Alert(Alert.AlertType.WARNING);
+    private Alert alertMess = new Alert(Alert.AlertType.INFORMATION);
 
     @Override
     public void start(Stage Stage) throws Exception {
@@ -46,12 +53,93 @@ public class MarcaBoundary extends Application {
         txtNome.relocate(50, 140);
 
         btnAdd.relocate(100, 200);
+        btnAlt.relocate(50,200);
+        btnRmv.relocate(300, 200);
+        btnCon.relocate(150, 200);
+        btnCan.relocate(150, 200);
 
-        pPane.getChildren().addAll(lblQuadrado, lblCodigo, txtCodigo, lblNome, txtNome, btnAdd);
+        btnAlt.setVisible(false);
+        btnCon.setVisible(false);
+        btnRmv.setVisible(false);
+        btnCan.setVisible(false);
+
+        btnRmv.getStylesheets().add(FuncionarioBoundary.class.getResource("btnExcluirStyle.css").toExternalForm());
+
+        pPane.getChildren().addAll(lblQuadrado, lblCodigo, txtCodigo, lblNome, txtNome, btnAdd, btnPesquisar, btnAlt,
+                btnRmv, txtPesquisar, btnCan, btnCon);
 
         btnAdd.setOnAction((e) -> {
-           marcaControl.adicionar(boundaryToEntity());
+           valido = marcaControl.ValidarCampos(txtCodigo.getText(), txtNome.getText());
+           if(valido){
+               marcaControl.adicionar(boundaryToEntity());
+               alertMess.setHeaderText("CADASTRADO COM SUCESSO!");
+               alertMess.showAndWait();
+               this.entityToBoundary(new Marca());
+           }else{
+               alertWarn.setHeaderText("INSIRA OS CAMPOS CORRETAMENTE");
+               alertWarn.showAndWait();
+           }
         });
+
+        btnPesquisar.setOnAction((e) ->{
+            Marca Mc = marcaControl.pesquisarPorCodigo(Integer.parseInt(txtPesquisar.getText()));
+            this.entityToBoundary(Mc);
+            if(Mc!= null){
+                btnPesquisar.setVisible(false);
+                txtPesquisar.setEditable(false);
+                txtCodigo.setEditable(false);
+                txtNome.setEditable(false);
+                btnAdd.setVisible(false);
+                btnAlt.setVisible(true);
+                btnRmv.setVisible(true);
+                btnCan.setVisible(true);
+            }
+        });
+
+        btnCan.setOnAction((event -> {
+            this.entityToBoundary(new Marca());
+            btnPesquisar.setVisible(true);
+            txtPesquisar.setEditable(true);
+            txtCodigo.setEditable(true);
+            txtNome.setEditable(true);
+            btnAdd.setVisible(true);
+            btnAlt.setVisible(false);
+            btnRmv.setVisible(false);
+            btnCan.setVisible(false);
+        }));
+
+        btnAlt.setOnAction((event -> {
+            btnAlt.setVisible(false);
+            btnRmv.setVisible(false);
+            btnCon.setVisible(true);
+            btnCan.setVisible(false);
+            txtNome.setEditable(true);
+        }));
+
+        btnRmv.setOnAction((event -> {
+            try {
+                marcaControl.excluir(boundaryToEntity());
+            }catch (Exception e1) {
+                e1.printStackTrace();
+            }
+            alertMess.setHeaderText("Funcionario excluido!");
+            alertMess.showAndWait();
+            btnCan.fire();
+        }));
+
+        btnCon.setOnAction((event -> {
+            valido = marcaControl.ValidarCampos(txtCodigo.getText(), txtNome.getText());
+            if (valido){
+                marcaControl.alterar(boundaryToEntity());
+                alertMess.setHeaderText("ALTERADO COM SUCESSO!");
+                alertMess.showAndWait();
+                btnCon.setVisible(false);
+                btnCan.fire();
+            } else {
+                alertWarn.setHeaderText("Preencha os campos corretamente!");
+                alertWarn.showAndWait();
+            }
+        }));
 
         Stage.setScene(scCeneMarca);
         Stage.setTitle("Marca S2");
@@ -61,8 +149,22 @@ public class MarcaBoundary extends Application {
 
     public Marca boundaryToEntity() { //jogando tudo que ta na tela, pra entidade
         Marca Mc = new Marca();
-        Mc.setCodigo(Integer.parseInt(txtCodigo.getText()));
         Mc.setNomeMarca(txtNome.getText());
+        try {
+            Mc.setCodigo(Integer.parseInt(txtCodigo.getText()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return Mc;
+    }
+
+    public void entityToBoundary(Marca Mc) { //consultando a entidade e jogando na tela
+        if (Mc != null) {
+            txtCodigo.setText(String.valueOf(Mc.getCodigo()));
+            txtNome.setText(Mc.getNomeMarca());
+        } else {
+            alertMess.setHeaderText("MARCA NÃO EXISTE.");
+            alertMess.showAndWait();
+        }
     }
 }
--- src/marca/MarcaController.java
+++ src/marca/MarcaController.java
@@ -1,9 +1,10 @@
 package marca;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import funcionario.Funcionario;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarcaController {
 
@@ -23,5 +24,61 @@ public class MarcaController {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
+
+    public Marca pesquisarPorCodigo(int txtPesquisar) {
+        try (Connection conPQM = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            String sqlPQM = "SELECT * FROM marca WHERE CODIGO_MARCA LIKE ?";
+            PreparedStatement stmtPQM = conPQM.prepareStatement(sqlPQM);
+            stmtPQM.setInt(1, txtPesquisar);
+            ResultSet rsM = stmtPQM.executeQuery();
+            while (rsM.next()) {
+                Marca Mc = new Marca();
+                Mc.setCodigo(rsM.getInt("CODIGO_MARCA"));
+                Mc.setNomeMarca(rsM.getString("NOME_MARCA"));
+                return Mc;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void excluir(Marca Mc) {
+        try (Connection conEX = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            String sqlEX = "DELETE FROM marca WHERE CODIGO_MARCA LIKE ?";
+            PreparedStatement stmtEX = conEX.prepareStatement(sqlEX);
+            stmtEX.setInt(1, Mc.getCodigo());
+            ResultSet rs = stmtEX.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void alterar(Marca Mc) {
+        try (Connection conAL = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            String sqlAL = "UPDATE marca SET " +
+                    "NOME_MARCA = ? where CODIGO_MARCA = ?";
+            PreparedStatement stmtAL = conAL.prepareStatement(sqlAL);
+            stmtAL.setString(1, Mc.getNomeMarca());
+            stmtAL.setInt(2, Mc.getCodigo());
+            int rs = stmtAL.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean ValidarCampos (String txtCodigo, String txtNomeMarca){
+        List<String> lista = new ArrayList<>();
+        lista.add(0, txtCodigo);
+        lista.add(1, txtNomeMarca);
+        for (String l : lista) {
+            if (l == null || l.equals("")) {
+                lista.clear();
+                return false;
+            }
+        }
+        lista.clear();
+        return true;
     }
 }
