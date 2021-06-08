529679065e4b3d3dddfd720a50db3229c9a404ab
Diego <Diegokoga1@gmail.com>
2021-06-08 02:58:11 -0300

    tabela vendas apresentando os valores

---
+++ src/vendas/Tabela.java
@@ -0,0 +1,48 @@
+package vendas;
+
+public class Tabela {
+    private int codigoProduto;
+    private String nomeProduto;
+    private int quantidade;
+    private double preco;
+
+    public Tabela(int codigoProduto, String nomeProduto,int quantidade,double preco){
+        this.codigoProduto = codigoProduto;
+        this.nomeProduto = nomeProduto;
+        this.quantidade = quantidade;
+        this.preco = preco;
+
+    }
+
+    public int getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public void setCodigoProduto(int codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
