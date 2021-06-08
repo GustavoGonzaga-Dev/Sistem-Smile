cc8df59105aaec33c34e2eaf317423914b00adc1
gustavo gonzaga <gustavogonzagafarias@gmail.com>
2021-06-08 19:18:21 -0300

    Sem SysOut

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -10,7 +10,6 @@ public class LoginController {
 
     public boolean validarLogin(Login Lg) {
         permissao = (loginDao.validarLogin(Lg));
-        System.out.println(permissao);
         return permissao.contains(Lg.getSenha());
     }
 
